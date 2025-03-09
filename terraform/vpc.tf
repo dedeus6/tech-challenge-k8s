b0268e50ee61fea320e1db6cc0816ba3cfd76e84
Filipe de Deus <filipedeus2015@gmail.com>
2025-03-09 11:46:13 -0300

    adding initial configs

---
+++ terraform/vpc.tf
@@ -0,0 +1,27 @@
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "fast-food-vpc"
+  }
+}
+
+resource "aws_subnet" "private_1" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "fast-food-private-subnet-1"
+  }
+}
+
+resource "aws_subnet" "private_2" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-east-1b"
+
+  tags = {
+    Name = "fast-food-private-subnet-2"
+  }
+}
