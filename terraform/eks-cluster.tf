ea11dafde172f24974262d009ca68e5388771b58
Filipe de Deus <filipedeus2015@gmail.com>
2025-03-09 16:29:00 -0300

    adding terraform files

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -1,34 +1,14 @@
-provider "aws" {
-  region = "us-east-1"
-}
+resource "aws_eks_cluster" "fast-food-eks" {
+  name     = "fast-food-eks-cluster"
+  role_arn = var.labRole
 
-resource "aws_eks_cluster" "eks_cluster" {
-  name     = "fast-food-cluster"
-  role_arn = aws_iam_role.eks_role.arn
 
   vpc_config {
-    subnet_ids = [aws_subnet.private_1.id, aws_subnet.private_2.id]
+    subnet_ids         = ["${var.subnetA}", "${var.subnetB}", "${var.subnetC}"]
+    security_group_ids = ["${var.securityGroupId}"]
   }
 
-  depends_on = [aws_iam_role_policy_attachment.eks_policy]
-}
-
-resource "aws_iam_role" "eks_role" {
-  name = "eks-cluster-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Action = "sts:AssumeRole"
-      Effect = "Allow"
-      Principal = {
-        Service = "eks.amazonaws.com"
-      }
-    }]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "eks_policy" {
-  role       = aws_iam_role.eks_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  access_config {
+    authentication_mode = var.accessConfig
+  }
 }
